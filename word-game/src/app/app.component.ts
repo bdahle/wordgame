a30f6a0fc9ef49504dbda7dddacb00fd064c0b3d
Bjørn Harbo Dahle <bdahle@hey.com>
2020-10-13 09:23:48 +0200

    red on lose

--- word-game/src/app/app.component.ts
+++ word-game/src/app/app.component.ts
@@ -12,21 +12,28 @@ export class AppComponent {
   title = 'word-game'
 
   word = ['h', 'u', 's']
+  tryCounter = 0;
 
   @HostListener('window:keydown', ['$event'])
   keyEvent(event: KeyboardEvent) {
     console.log(event)
+    this.tryCounter++
 
     for (const [i, char] of this.word.entries()) {
       if (char == event.key) {
         this.word.splice(i, 1)
+        this.tryCounter--
       }
     }
     console.log(this.word)
 
+
     if (this.word.length == 0) {
       console.log("Du vant!")
       this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor = 'green'
+    } else if (this.tryCounter >= 5) {
+      console.log("Du tapte")
+      this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor = 'red'
     }
   }
 

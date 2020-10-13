50fd2d91c29a0ad11bf8f95fbca9c2b08649d394
Bjørn Harbo Dahle <bdahle@hey.com>
2020-10-13 11:46:55 +0200

    fixed win lose logic, cleanup

--- word-game/src/app/app.component.ts
+++ word-game/src/app/app.component.ts
@@ -1,5 +1,7 @@
+//TODO ignorere taster som ikke er bokstaver
+//TODO virker med store bokstaver
+
 import { Component, ElementRef, HostListener } from '@angular/core';
-import { stringify } from 'querystring';
 
 @Component({
   selector: 'app-root',
@@ -8,33 +10,36 @@ import { stringify } from 'querystring';
 })
 export class AppComponent {
   constructor(private elementRef: ElementRef) { }
-
-
   title = 'word-game'
 
-  // word = ['h', 'u', 's']
   word = "hus"
+  guessedWord = "   "
+  remainingWord = ""
   tryCounter = 0;
 
-  @HostListener('window:keydown', ['$event'])
-  keyEvent(event: KeyboardEvent) {
-    console.log(event)
-    this.tryCounter++
+  ngOnInit(): void {
+    this.setRemainingWord()
+    this.logWords()
+  }
 
-    // for (const [i, char] of this.word.entries()) {
-    // for (let i in this.word) {
+  logWords() {
+    console.log("WORD", this.word)
+    console.log("GUESSED '" + this.guessedWord + "'")
+    console.log("REMAIN", this.remainingWord)
+  }
+
+  setRemainingWord() {
     for (let i = 0; i < this.word.length; i++) {
-
-      if (this.word[i] == event.key) {
-        // this.word.slice(i, 1)
-        this.word = this.word.slice(0, i) + this.word.slice(i + 1)
-        this.tryCounter--
+      if (this.guessedWord[i] == this.word[i]) {
+        this.remainingWord = this.remainingWord.concat(" ")
+      } else {
+        this.remainingWord = this.remainingWord.concat(this.word[i])
       }
     }
-    console.log(this.word)
+  }
 
-
-    if (this.word.length == 0) {
+  hasWonOrLost() {
+    if (this.guessedWord == this.word) {
       console.log("Du vant!")
       this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor = 'green'
     } else if (this.tryCounter >= 5) {
@@ -42,4 +47,44 @@ export class AppComponent {
       this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor = 'red'
     }
   }
+
+  setCharAt(str, index, chr) {
+    if (index > str.length - 1) return str;
+    return str.substring(0, index) + chr + str.substring(index + 1);
+  }
+
+  @HostListener('window:keydown', ['$event'])
+  keyEvent(event: KeyboardEvent) {
+    console.log(event)
+    this.tryCounter++
+
+
+    for (let i = 0; i < this.remainingWord.length; i++) {
+      if (this.remainingWord[i] == event.key) {
+        this.guessedWord = this.setCharAt(this.guessedWord, i, this.remainingWord[i])
+        this.remainingWord = this.setCharAt(this.remainingWord, i, " ")
+        this.tryCounter--
+
+        // this.remainingWord = this.replaceChar(this.remainingWord, "X", i)
+      } else if (this.guessedWord[i] == event.key) {
+        this.tryCounter--
+
+      }
+    }
+
+
+    this.logWords()
+    this.hasWonOrLost()
+  }
+
+
 }
+
+    // for (let i = 0; i < this.remainingWord.length; i++) {
+    //   if (this.remainingWord[i] == event.key) {
+    //     this.guessedWord = this.guessedWord.slice(0, i) + this.word[i] + this.guessedWord.slice(i + 1)
+    //     this.remainingWord = this.remainingWord.slice(0, i) + this.remainingWord.slice(i + 1)
+    //     this.tryCounter--
+    //   }
+    // }
+    // this.updateWords()
